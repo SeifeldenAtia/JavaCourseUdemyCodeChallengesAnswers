2f8c1199ad81c040dcfadfac6de24c300d04a77c
Seifelden Atia <68141338+SeifeldenAtia@users.noreply.github.com>
2023-06-06 16:00:17 +0200

    Add files via upload

---
+++ Section 8/Coding Exercise 40 Polymorphism Answer Car/Car.java
@@ -0,0 +1,36 @@
+public class Car {
+
+    private boolean engine ;
+    private int cylinders ;
+    private String name;
+    private int wheels ;
+
+    public Car( int cylinders, String name) {
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+    }
+
+    public String startEngine() {
+        return (getClass().getSimpleName() + " -> startEngine") ;
+    }
+
+    public String accelerate  () {
+        return ( getClass().getSimpleName() + " -> accelerate ");
+    }
+
+    public String brake  () {
+        return ( getClass().getSimpleName() + " -> brake ");
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+
---
+++ Section 8/Coding Exercise 40 Polymorphism Answer Car/Holden.java
@@ -0,0 +1,19 @@
+public class Holden extends Car{
+
+
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
